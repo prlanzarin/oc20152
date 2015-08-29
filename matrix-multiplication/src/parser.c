adb379a37bb8abb52d1c0c80ed8b7d4bcd3d5ee4
prlanzarin <prlanzarin@inf.ufrgs.br>
2015-08-29 20:49:26 -0300

    parser init

--- matrix-multiplication/src/parser.c
+++ matrix-multiplication/src/parser.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include <parser.h>
 
+int **matrix_alloc(int rows, int cols, int **matrix);
+
+int **matrix_parse(char *filename, int **matrix);
+
+int **matrix_parse(char *filename, int **matrix) {
+	int rows, cols, i;
+	FILE *file;
+	char *buffer;
+	file = fopen(filename, r);
+		
+
+
+}
 int main(int argc, char *argv[]) {
 
 	printf("Hello %s \n", *argv);
